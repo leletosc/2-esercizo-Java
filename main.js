08c4759c37480f4145319dab10f355cced43b332
Iceman <daniele_toscano@yahoo.it>
2023-05-26 17:46:49 +0200

    Conclusione esercizio

---
+++ main.js
@@ -0,0 +1,49 @@
+/*
+
+I datatypes sono:
+number, string, boolean, undefined,null
+I datatype sono valori inseriti in variabili, i numeri sono valori nuerici,
+string sono parole o numeri racchiusi tra virgolette, 
+boolean si dividono tra TRUE E FALSE.
+Undefined lo definisce javascript di default ed indica una variabile senza contenuto a differenza del 
+NULL che indica la stessa cosa ma viene definito dall'utente.
+
+*/
+
+
+let prova = null
+console.log(typeof prova);
+
+let somma = 12 + 20
+
+console.log(somma)
+
+
+let x = 12
+let Name = 'Daniele'
+console.log(x)
+x -= 4
+console.log(x)
+
+const io {
+    nome:'Daniele'
+    cognome: 'Toscano'
+    Hobby:'Body building'
+           
+}
+
+
+
+let name1 = 'john'
+let name2 = 'John'
+console.log( name1 === name2)
+
+console.log.(name1.toLowerCase === name2.toLowerCase)
+
+
+
+
+
+
+
+
